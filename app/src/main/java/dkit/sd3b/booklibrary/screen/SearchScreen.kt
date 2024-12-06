936b45a5088b035dd4813ece84c3516966eb93de
eljesam <124920260+eljesam@users.noreply.github.com>
2024-12-06 15:35:58 +0000

    Made search function more interactive. Need to check two values

--- app/src/main/java/dkit/sd3b/booklibrary/screen/SearchScreen.kt
+++ app/src/main/java/dkit/sd3b/booklibrary/screen/SearchScreen.kt
@@ -1,7 +1,7 @@
 package dkit.sd3b.booklibrary.screen
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -9,10 +9,19 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import dkit.sd3b.booklibrary.model.Book
+import dkit.sd3b.booklibrary.model.BookViewModel
+
+private var <E> List<E>.value: List<E>
+    get() {
+        return this
+    }
+    set(value) {}
 
 @Composable
-fun SearchScreen(navController: NavController) {
+fun SearchScreen(viewModel: BookViewModel, navController: NavController) {
     var searchQuery by remember { mutableStateOf("") }
+    var searchResults by remember { mutableStateOf(listOf<Book>()) }
 
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Text("Search for Books", style = TextStyle(fontSize = 24.sp))
@@ -29,12 +38,26 @@ fun SearchScreen(navController: NavController) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = {performSearch(searchQuery)}, modifier = Modifier.fillMaxWidth()) {
+        Button(onClick = { performSearch(searchQuery, viewModel, searchResults) }, modifier = Modifier.fillMaxWidth()) {
             Text("Search")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn {
+            items(searchResults) { book ->
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = book.title, style = MaterialTheme.typography.titleSmall)//check this
+                    Text(text = book.author, style = MaterialTheme.typography.bodyLarge)//check this
+                }
+            }
         }
     }
 }
 
-fun performSearch(searchQuery: String) {
-    Log.d("SearchScreen", "Searching for: $searchQuery")
+fun performSearch(query: String, viewModel: BookViewModel, searchResults: List<Book>) {
+    // Implement your search logic here
+    viewModel.searchBooks(query) { results ->
+        searchResults.value = results
+    }
 }
